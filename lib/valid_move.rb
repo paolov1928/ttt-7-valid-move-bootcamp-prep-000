4062d010659c3646cccdc98532f85c67a4c4ac55
ventura.paolo <ventura.paolo@gmail.com>
2018-09-18 14:16:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if index.between?(0, 8)
-    return true
-  elsif position_taken?(board, index)
+  if position_taken?(board, index)
     return false
+  elsif index.between?(0, 8)
+    return true
   else
     return false
 end
